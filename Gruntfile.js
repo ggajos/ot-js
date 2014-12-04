74c9c1fe15c98480e4a7395492dd58055b305e4c
Grzegorz Gajos <grzegorz.gajos@opentangerine.com>
2014-12-04 10:17:12 +0100

    updated build to use grunt and qunit

---
+++ Gruntfile.js
@@ -0,0 +1,65 @@
+module.exports = function (grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            },
+            testRelease: {
+                src: ['src/**/*.js'],
+                dest: 'test/qunit/build/<%= pkg.name %>.js'
+            },
+            testSuite: {
+                src: ['test/unit/**/*.js'],
+                dest: 'test/qunit/build/<%= pkg.name %>-testsuite.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+            },
+            build: {
+                src: ['src/_header.js', 'src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.min.js'
+            }
+        },
+        qunit: {
+            all: ['test/qunit/index.html']
+        },
+        connect: {
+            server: {
+                options: {
+                    livereload: true,
+                    port: 8234,
+                    base: 'test/qunit'
+                }
+            }
+        },
+        watch: {
+            test: {
+                files: ['src/**', 'test/unit/**'],
+                tasks: ['concat', 'jshint', 'qunit']
+            }
+        },
+        jshint: {
+            all: ['Gruntfile.js', 'src/**/*.js', 'test/unit/**/*.js']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // Default task(s).
+    grunt.registerTask('default', ['concat', 'connect:server', 'watch']);
+    grunt.registerTask('release', ['concat', 'uglify']);
+};
--- test/unit/z-after.js
+++ test/unit/z-after.js
@@ -2,32 +2,31 @@
     'use strict';
     ot.logLevel.withNoLog(function() {
         tests.forEach(function (testGroup) {
-            describe(['ot', testGroup.name].join('.'), function () {
-                testGroup.tests.forEach(function (testCase) {
-                    if (testCase.run !== undefined) {
-                        if (testGroup.before) {
-                            testGroup.before();
-                        }
-                        ot.log().debug('Executing ' + testCase.name());
-                        var result = testCase.run();
-                        it(testCase.name(), function () {
-                            expect(true).toBe(result.isSuccess());
-                        });
-                        if (!result.isSuccess()) {
-                            ot.logLevel.withLogDebug(function () {
-                                ot.log().debug([
-                                    'executing test (',
-                                    testCase.name(),
-                                    ') with more verbose logging'
-                                ].join());
-                                ot.log().debug([
-                                    'expected result:'
-                                ].join());
-                                testCase.run();
-                            });
-                        }
+            QUnit.module(['ot', testGroup.name].join('.'));
+            testGroup.tests.forEach(function (testCase) {
+                if (testCase.run !== undefined) {
+                    if (testGroup.before) {
+                        testGroup.before();
                     }
-                });
+                    ot.log().debug('Executing ' + testCase.name());
+                    var result = testCase.run();
+                    QUnit.test(testCase.name(), function (assert) {
+                        assert.ok(true === result.isSuccess(), testCase.name());
+                    });
+                    if (!result.isSuccess()) {
+                        ot.logLevel.withLogDebug(function () {
+                            ot.log().debug([
+                                'executing test (',
+                                testCase.name(),
+                                ') with more verbose logging'
+                            ].join());
+                            ot.log().debug([
+                                'expected result:'
+                            ].join());
+                            testCase.run();
+                        });
+                    }
+                }
             });
         });
     });
